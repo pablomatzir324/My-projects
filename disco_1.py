3b3564c1d7827200bb36ddf315ea4619cc46887d
PabloMatzir <pablomatzir324@gmail.com>
2026-07-03 22:56:54 -0600

    Se añadió una nueva entrada al .gitignore y el archivos .py del disco 1

---
+++ disco_1.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from frank.io import load_uvtable, save_fit
+from frank.radial_fitters import FrankFitter
+from frank.geometry import FitGeometryGaussian
+from frank.make_figs import make_quick_fig
+
+
+uvtable_filename = "C:\Archivos\Python\Frank\disco 1\J16120_Band7_keepflagsFalse.txt"
+u, v, vis, weights = load_uvtable(uvtable_filename)
+
+FF = FrankFitter(Rmax=1.6, N=250, geometry=FitGeometryGaussian(),
+                 alpha=1.05, weights_smooth=1e-4)
+
+sol = FF.fit(u, v, vis, weights)
+
+fig, axes = make_quick_fig(u, v, vis, weights, sol, bin_widths=[1e3, 5e4], force_style=True)
+plt.show()
